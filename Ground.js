f3d38ff4e2c268bddc51808b33bcd6a799822c0c
CoderKhushi <74098205+CoderKhushi@users.noreply.github.com>
2020-12-12 15:52:39 +0530

    Add files via upload

---
+++ Ground.js
@@ -0,0 +1,13 @@
+class Ground extends BaseClass {
+
+      constructor(x, y, width, height) {
+        super(x,y,width,height)
+        this.image=loadImage("sprites/ground.png")
+         }
+         display(){
+           
+       super.display()
+       
+         }
+       
+  }
---
+++ Pig.js
@@ -0,0 +1,12 @@
+class Pig extends BaseClass{
+  constructor(x, y, width, height) {
+ super(x,y,width,height)
+ this.image=loadImage("sprites/wood1.png")
+  }
+  display(){
+    
+super.display()
+
+  }
+};
+  
